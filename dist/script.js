cfdea9ac13284887f6965853582421c9f3571778
Tremar Thomas <trthomas@icstars.org>
2024-07-10 08:30:37 -0500

    Finish player functionality, add styling

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,15 @@
 const playlistIDs = [
   "PL7LTTBAsxHWmKv5j3RUf9ak46s_6pMZdz",
+  "PL7LTTBAsxHWkoCF_O8zMSy29ZusIiDEe5",
   "PL7LTTBAsxHWknggni3ko8elaHst_1YYtY",
+  "PL7LTTBAsxHWmKMQAoY2sEJDDJ9ypDDSua",
+  "PL7LTTBAsxHWmtpXpNc02L7JPitjpf4ioj",
 ];
+
 const playlistWrapper = document.querySelector(".playlist-wrapper");
+const trackPlayer = document.querySelector(".track-player > iframe");
+let playerHidden = true;
+
 async function displayVideos() {
   playlistIDs.forEach(async (ID) => {
     const data = await fetch(
@@ -20,23 +27,32 @@ async function displayVideos() {
 
     ancherElem.append(playlistHeader);
     playlistWrapper.append(ancherElem, trackListWrapper);
-
     data.items.forEach((songObj) => {
       const wrapper = document.createElement("div");
-      const img = document.createElement("img");
+      const coverArt = document.createElement("div");
       const trackTitle = document.createElement("h4");
 
       wrapper.classList.add("track-wrapper");
-      img.classList.add("cover-art");
-      img.src = songObj.thumbnail;
+      coverArt.classList.add("cover-art");
+      coverArt.setAttribute("style", `background-image: url(${songObj.thumbnail})`)
       trackTitle.textContent = songObj.title;
 
-      wrapper.append(img, trackTitle);
+      wrapper.addEventListener("click", (e) => {
+        updatePlayer(songObj.link)
+      })
+
+      wrapper.append(coverArt, trackTitle);
       trackListWrapper.append(wrapper);
     });
-
-    console.log(data);
   });
+}
+
+function updatePlayer(url) {
+  if (playerHidden) {
+    playerHidden = false;
+    document.querySelector(".track-player").classList.toggle("hide");
+  }
+  trackPlayer.src = "https://www.youtube.com/embed/" + url.slice(url.indexOf("=") + 1) + "?controls=0&autoplay=1";
 }
 
 displayVideos();
